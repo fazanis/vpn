633725f2835eb24ba76009d4db3aa0c6ada29786
fazan <web.master.88@mail.ru>
2026-04-18 10:39:43 +0500

    исправлена ошибка модального окна добавления устройства

--- resources/views/cabinet/layouts/app.blade.php
+++ resources/views/cabinet/layouts/app.blade.php
@@ -107,8 +107,6 @@
 
     @include('cabinet.partials.footer')
 
-    <!-- Подключаем JS Bootstrap для работы мобильного меню -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
