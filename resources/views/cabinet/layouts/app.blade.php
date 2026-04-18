145a80f82f45439f131eac8f4eea2fc81a0bcc01
fazan <web.master.88@mail.ru>
2026-04-18 10:36:48 +0500

    исправлена ошибка модального окна добавления устройства

--- resources/views/cabinet/layouts/app.blade.php
+++ resources/views/cabinet/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>{{ env('APP_NAME') }} - Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <style>
         body {
             background: #0b1c2c;
