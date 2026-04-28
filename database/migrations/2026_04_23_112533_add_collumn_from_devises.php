e4dcc2f22649ab8939c89131e324e7ddf2e3c9bd
fazan <web.master.88@mail.ru>
2026-04-28 11:43:20 +0500

    Исправлено поле trafik

--- database/migrations/2026_04_23_112533_add_collumn_from_devises.php
+++ database/migrations/2026_04_23_112533_add_collumn_from_devises.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('devises', function (Blueprint $table) {
-            $table->string('trafik')->after('ui_name')->default(0);
+            $table->bigInteger('trafik')->after('ui_name')->default(0);
         });
     }
 
