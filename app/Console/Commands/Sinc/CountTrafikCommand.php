d6497ef75c2d88d778ef75ef435d1e1e38c41314
fazan <web.master.88@mail.ru>
2026-09-07 16:30:36 +0500

    Исправлен подсчет трафика для панели v2 v3

--- app/Console/Commands/Sinc/CountTrafikCommand.php
+++ app/Console/Commands/Sinc/CountTrafikCommand.php
@@ -38,9 +38,26 @@ class CountTrafikCommand extends Command
 //
         }
 //        dd($result);
-        foreach($result as $key=>$value){
-            $devise=Devise::query()->where('ui_id',$key)->update(['trafik' => $value]);
-            dump($devise.' '.$value);
+        foreach ($result as $key => $value) {
+            $devise = Devise::query()
+                ->where('ui_id', $key)
+                ->first();
+
+            dump([
+                'id' => $devise?->id,
+                'ui_id' => $key,
+                'before' => $devise?->trafik,
+                'value' => $value,
+                'value_type' => gettype($value),
+            ]);
+
+            $devise?->update([
+                'trafik' => (string) $value,
+            ]);
+
+            dump([
+                'after' => $devise?->fresh()->trafik,
+            ]);
         }
     }
 }
